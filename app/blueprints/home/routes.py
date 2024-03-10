04e7c3432d3b39f8972f0239236159ced0dbea11
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-03-10 09:51:12 +0100

    add home page (starting one) with 'logo'

--- app/blueprints/home/routes.py
+++ app/blueprints/home/routes.py
@@ -2,7 +2,7 @@ from app.blueprints.home import home
 from flask import render_template
 
 
-@home.route('/', methods=["GET", "POST"])
-@home.route('/home', methods=["GET", "POST"])
-def main_view():
+@home.route("/", methods=["GET", "POST"])
+@home.route("/home", methods=["GET", "POST"])
+def home_page():
     return render_template("home.html")
