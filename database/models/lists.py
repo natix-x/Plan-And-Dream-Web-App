c4fbd11295f1aed14d9358b52386b01758960343
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-03-13 18:41:21 +0100

    create lists module which is connected with users

--- database/models/lists.py
+++ database/models/lists.py
@@ -1,13 +1,15 @@
 from app import db
 
 
-class ToDo(db.Model):
+class Lists(db.Model):
     """
-    defines "to do" table that contains two columns: text - for defining planned activity and
-    complete - for marking if it is done or not
+    defines table called "lists" that contains 3 columns: id, text and user_id
+    id: primary key
+    text: "to do list"'s title
+    user_id: id of the owner of the particular list based on table "users"
     """
 
-    __tablename__ = "todo"
+    __tablename__ = "lists"
     id = db.Column(db.Integer(), primary_key=True)
     text = db.Column(db.String(200))
-    complete = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
--- database/models/users.py
+++ database/models/users.py
@@ -1,13 +1,23 @@
 from app import db
 from flask_login import UserMixin
+from flask_bcrypt import generate_password_hash
+from .lists import Lists
 
 
 class User(db.Model, UserMixin):
     """
-    defines table called "users" which contains 3 columns: username, email_address and password_hash
+    defines table called "users" which contains 4 columns: id, username, email_address and password,
+    this model is related to 'Lists'
     """
+
     __tablename__ = "users"
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
-    password_hash = db.Column(db.String(length=60), nullable=False)
+    password = db.Column(db.String(length=60), nullable=False)
+    lists = db.relationship("Lists", backref="user", lazy=True)
+
+    def __init__(self, username, email_address, password):
+        self.username = username
+        self.email_address = email_address
+        self.password = generate_password_hash(password).decode("utf-8")
