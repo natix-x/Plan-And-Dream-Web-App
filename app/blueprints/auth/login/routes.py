9a3e2c3700ed7c38f2f8dbfcb163997f009a4262
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-03-25 20:01:03 +0100

    add logout functionalities and all functions descriptions

--- app/blueprints/auth/login/routes.py
+++ app/blueprints/auth/login/routes.py
@@ -2,27 +2,54 @@ from app.blueprints.auth.login import login
 from app.blueprints.auth.forms import LoginForm
 from flask import render_template, request, redirect, url_for, flash
 from app import login_manager, db
-from database.models.users import User
-from flask_login import login_user
+from database.models.users import Users
+from flask_login import login_user, current_user, logout_user
 from flask_bcrypt import check_password_hash
 
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    """
+    given user_id returns the associated User object
+    :param user_id: user id to retrieve
+    :return: User object
+    """
+    return Users.query.get(user_id)
 
 
 @login.route("/login", methods=["GET", "POST"])
 def login_page():
+    """
+    displays login form and:
+    if user is already registered and email and password match: redirect user to his unique page and
+    displays success message;
+    if user is already registered and email and password do not match or
+    user is not registered: displays error message;
+    :return: flash message
+    """
     form = LoginForm()
     if request.method == "POST" and form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
+        user = Users.query.filter_by(username=form.username.data).first()
         if user:
-            if check_password_hash(user.password_hash, form.password.data):
-                login_user(user, remember=True)
+            if check_password_hash(user.password, form.password.data):
+                login_user(user)
                 flash(f'Success! Now you are now logged as {user.username}.', 'success')
-                return redirect(url_for("user_page.user_page", username=user.username))
+                return redirect(url_for("user_page.user_page_view", username=current_user.username))  # redirect user to user's main page
             else:
                 flash("Username and password don't match.", "danger")
+        else:
+            flash("Nickname does not exist.", "danger")
 
     return render_template("login.html", form=form)
+
+
+@login.route("/logout")
+def logout():
+    """
+    logs user out
+    :return: flashed message 'You are logged out!'
+    """
+    logout_user()
+    flash("You are logged out!", "danger")
+    return redirect(url_for('home.home_page'))
+
--- app/blueprints/auth/register/routes.py
+++ app/blueprints/auth/register/routes.py
@@ -1,27 +1,34 @@
 from app.blueprints.auth.register import register
 from app.blueprints.auth.forms import RegistrationForm
 from flask import render_template, request, redirect, url_for, flash
-from database.models.users import User
+from database.models.users import Users
 from app import db, bcrypt
+from flask_login import login_user
 
 
 @register.route("/register", methods=["GET", "POST"])
 def register_page():
+    """
+    displays registration form and:
+    if username and email is already registered displays error messages;
+    else: creates new user, adds him to db (table 'users'), log him in, displays success message and redirect him to user unique page
+    :return: flash message
+    """
     form = RegistrationForm()
     if request.method == "POST" and form.validate_on_submit():
-        if User.query.filter_by(username=form.username.data).first():  # check if username already exists in db
+        if Users.query.filter_by(username=form.username.data).first():  # check if username already exists in db
             flash('Username already registered!', 'danger')
-        elif User.query.filter_by(email_address=form.email_address.data).first():  # check if email already exists in db
+        elif Users.query.filter_by(email_address=form.email_address.data).first():  # check if email already exists in db
             flash("Email already registered!", 'danger')
         else:
-            new_user = User(
+            new_user = Users(
                 username=form.username.data,
                 email_address=form.email_address.data,
-                password_hash=bcrypt.generate_password_hash(form.password1.data).decode("utf-8"),  # hash inserted password
-            )
+                password=form.password1.data)
             db.session.add(new_user)
             db.session.commit()  # add new user to db and commit changes
-            flash('Account created successfully!', 'success')
-            return redirect(url_for("user_page.user_page", username=new_user.username))  # redirect user to user's main page
+            login_user(new_user)
+            flash(f'Account created successfully! Now you are now logged as {new_user.username}.', 'success')
+            return redirect(url_for("user_page.user_page_view", username=new_user.username))  # redirect user to user's main page
 
     return render_template("register.html", form=form)
