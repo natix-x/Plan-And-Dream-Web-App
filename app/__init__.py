baa10ddad950b702fbfee6ecc1643c35fda61fa8
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-08-24 18:21:25 +0200

    added <configuration.py> which contains all configuration settings and connected it to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,19 @@
-from flask import Flask
+from configuration import Config
 from database import db, ma
+from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)  # Flask instance creation
+app.config.from_object(Config)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-app.config['SECRET_KEY'] = 'f3cfe9ed8fae309f02079dbf'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///plananddream.db'
+
 # defined directory for database: "Plan-And-Dream-Web-App"
 db.init_app(app)  # connect database and app
 ma.init_app(app)  # connect marshmallow and app
 
-from app.cli.db_config import db_create, db_drop
+# from app.cli.db_config import db_create, db_drop
 
 # blueprints imports and registration
 
@@ -21,13 +21,15 @@ from app.blueprints.home import home
 from app.blueprints.auth.register import register
 from app.blueprints.auth.login import login
 from app.blueprints.user_page import user_page
+from app.blueprints.db_models.lists import lists
+from app.blueprints.db_models.things_to_do import things_to_do
+from app.blueprints.db_models.users import users
 
 
 app.register_blueprint(home)
 app.register_blueprint(register)
 app.register_blueprint(login)
 app.register_blueprint(user_page)
-
-
-
-
+app.register_blueprint(lists)
+app.register_blueprint(things_to_do)
+app.register_blueprint(users)
---
+++ configuration.py
@@ -0,0 +1,7 @@
+class Config:
+    """
+    configuration settings
+    """
+    DEBUG = True
+    SECRET_KEY = "uRo4wKfxfjQ04gqKPD33Kw"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///plananddream.db"
